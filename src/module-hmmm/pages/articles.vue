83115ec0a712e194275c37f09f4251b10ca848a7
YuanS1119 <1650216381@qq.com>
2019-08-30 11:17:18 +0800

    hmmm的面试技巧页面搭建完成

--- src/module-hmmm/pages/articles.vue
+++ src/module-hmmm/pages/articles.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="dashboard-container">
-    
+    <el-row>
+      <el-button type="info" plain>新增面试技巧</el-button>
+    </el-row>
+    <el-row class="search">
+      <div class="left">
+        <label>关键字</label>
+        <el-input class="input" placeholder="请输入题目编号/题干"></el-input>
+      </div>
+      <div class="right">
+        <el-button>清除</el-button>
+        <el-button type="primary">搜索</el-button>
+      </div>
+    </el-row>
+    <el-row>
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column prop label="序号" width="100"></el-table-column>
+        <el-table-column prop label="标题" width="550"></el-table-column>
+        <el-table-column prop label="阅读数" width="100"></el-table-column>
+        <el-table-column prop label="状态" width="100"></el-table-column>
+        <el-table-column prop label="录入人" width="100"></el-table-column>
+        <el-table-column label="操作" width="220">
+          <template>
+            <el-button type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-row>
   </div>
 </template>
 
@@ -8,10 +35,41 @@
 export default {
   name: 'ArticlesList',
   data() {
-    return {}
+    return {
+      tableData: {}
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang='less' scoped>
+.dashboard-container {
+  padding: 20px;
+  background-color: #fff;
+  .search {
+    margin: 15px 0;
+    .left {
+      float: left;
+      label {
+        font-size: 14px;
+        width: 48px;
+        height: 28px;
+        line-height: 28px;
+        text-align: center;
+        color: #666;
+        font-weight: 400;
+        background-color: rgba(242, 242, 242, 1);
+      }
+      .input {
+        width: 152px;
+        height: 28px;
+        border-radius: 4px;
+        font-size: 14px;
+      }
+    }
+    .right {
+      float: right;
+    }
+  }
+}
 </style>
